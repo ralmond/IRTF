edd00f4ebf661b28ed31464fd3f5ccbb098976e3
Russell Almond <russell.g.almond@gmail.com>
2024-10-17 18:53:51 +0000

    Add Roxygen shell.

--- R/IRT.R
+++ R/IRT.R
@@ -1,9 +1,19 @@
 ## IRT Models
 
+
 IRToutcomes <- function (model) {
   model$outcomes()
 }
 
+#' Gets/sets the parameters of the model
+#'
+#' @param model An object of class IRTModel
+#'
+#' @return a list of parameters
+#' @export
+#'
+#'
+#' @examples
 IRTparams <- function (model) {
   model$params()
 }
@@ -16,11 +26,30 @@ IRTllike <- function(model,thetas,values=IRToutcomes(model)) {
   model$llike(thetas,values)
 }
 
+#' Logistic and inverse logistic function
+#'
+#' The function `logit` is the logistic function, $\log p/(1-p)$.
+#' The function `ilogit` is the inverse logistic function,
+#' $1/(1+e^{-x})$.  The function `lilogit` is the negative
+#' log of the inverse logit
+#'
+#'
+#' @param p -- The probability value to transform to theta space.
+#' @param x -- The theta value to transform into probability space.
+#'
+#' @return For `logit` the scale value for `ilogit`
+#' (`lilogit`) the (log) of the probability.
+#' @export
+#'
+#' @examples
 logit <- function (p) {log(p/(1-p))}
+#' @describeIn logit Inverse logit
 ilogit <- function (x) {1/(1+exp(-x))}
+#' @describeIn logit log inverse logit
 lilogit <- function (x) {-log(1+exp(-x))}
 
 ###
+
 
 IRT2PL <- R6Class(
   classname="IRT2PL",
